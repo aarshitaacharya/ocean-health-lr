b26392a55cd122d82002ff53eb8fea5c520923fa
aarshitaacharya <acharyaaarshita@gmail.com>
2025-06-10 18:41:20 -0500

    linked backend and frontent

--- ocean-health-ui/src/components/InputForm.jsx
+++ ocean-health-ui/src/components/InputForm.jsx
@@ -11,7 +11,7 @@ function InputForm({ onResult, formData, setFormData }) {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await fetch('http://127.0.0.1:5000/predict', {
+      const res = await fetch('https://ocean-health-lr.onrender.com/predict', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
